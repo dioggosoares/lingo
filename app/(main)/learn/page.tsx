a198cfacca71588f8e262443c2e39ae3e18c6071
Diogo Soares <dioggo.soares@live.com>
2024-03-27 15:15:20 -0300

    feat: create loading state for pages

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,10 +1,21 @@
+import { redirect } from 'next/navigation'
+
 import { FeedWrapper } from '@/components/feed-wrapper'
 import { StickyWrapper } from '@/components/sticky-wrapper'
+import { getUserProgress } from '@/db/queries'
 
 import { Header } from './_components/header'
 import { UserProgress } from './_components/user-progress'
 
-export default function LearnPage() {
+export default async function LearnPage() {
+  const userProgressData = getUserProgress()
+
+  const [userProgress] = await Promise.all([userProgressData])
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses')
+  }
+
   return (
     <div className="flex flex-row-reverse gap-12 px-6">
       <StickyWrapper>
