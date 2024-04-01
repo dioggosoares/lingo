57713ee5acf12a0229dcb3deb67546f1b8c3710b
Diogo Soares <dioggo.soares@live.com>
2024-04-01 15:35:11 -0300

    feat: create initial leaderboard page

--- app/(main)/leaderboard/page.tsx
+++ app/(main)/leaderboard/page.tsx
@@ -8,7 +8,7 @@ import { getUserProgress, getUserSubscription } from '@/db/queries'
 import { UserProgress } from '../learn/_components/user-progress'
 import { Items } from './_components/items'
 
-export default async function ShopPage() {
+export default async function LeaderboardPage() {
   const userProgressData = getUserProgress()
   const userSubscriptionData = getUserSubscription()
 
@@ -33,18 +33,19 @@ export default async function ShopPage() {
       </StickyWrapper>
       <FeedWrapper>
         <div className="flex w-full flex-col items-center">
-          <Image src="/shop.svg" alt="Shop" height={90} width={90} />
+          <Image
+            src="/leaderboard.svg"
+            alt="Leaderboard"
+            height={90}
+            width={90}
+          />
           <h1 className="text-neutral-800m my-6 text-center text-2xl font-bold">
-            Compras
+            Liderança
           </h1>
           <p className="mb-6 text-center text-lg text-muted-foreground">
-            Gaste seus pontos com coisas legais.
+            Veja sua posição entre outros alunos da comunidade.
           </p>
-          <Items
-            hearts={userProgress.hearts}
-            points={userProgress.points}
-            hasActiveSubscription={isPro}
-          />
+          {/* TODO: Add User List */}
         </div>
       </FeedWrapper>
     </div>
